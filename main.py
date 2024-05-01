87524437f5b9725c7a4f722420b12aee35379e1f
lucas <lucasssi89@gmail.com>
2024-05-01 00:57:38 -0300

    trabalhando com display

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import sys
 from PySide6.QtWidgets import QApplication, QLabel
 from main_window import MainWindon
+from PySide6.QtGui import QIcon
+from variables import WINDOW_ICON_PATH
+from display import Display
+
+
 
 if __name__ == '__main__':
+
+    #snake_case
+    #PascalCase
+    #camelCase
+
+
+    # cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindon()
 
-    label1 = QLabel('O meu texto')
-    label1.setStyleSheet('font-size: 150px')
-    window.v_layout.addWidget(label1)
+    # Definir o ícone
+    icon = QIcon(str(WINDOW_ICON_PATH)) 
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
+    # Display
+    display  = Display()
+    window.addToVLayout(display)
+
+    
+    # Executa tudo
     window.adjustFixedSize()
-
     window.show()
-
     app.exec()
 
--- main_window.py
+++ main_window.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import  QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtWidgets import  QMainWindow, QVBoxLayout, QWidget, QLineEdit, QTextEdit
+from PySide6.QtGui import QIcon
 
 class MainWindon(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
@@ -9,14 +10,18 @@ class MainWindon(QMainWindow):
 
         #Configurando o  layout básico
         self.cw = QWidget()
-        self.v_layout = QVBoxLayout()
-        self.cw.setLayout(self.v_layout)
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
         self.setCentralWidget(self.cw)
 
         #Titulo da janela
         self.setWindowTitle('CALCULADORA')
 
+
     def adjustFixedSize(self):
         # Última coisa a ser feita
         self.adjustSize()
         self.setFixedSize(self.width(), self.height())
+
+    def addToVLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)
